e3ed42a7fb0fede1f761bb34906db7eac6e755e4
tianrui0717 <1156184421@qq.com>
2016-06-07 17:14:58 +0800

    testgit1

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,20 +1,9 @@
 
-	/**
-	 * 
-	 */
+	
 	function calculate_median(arr) {
 	  //请勿改动函数名
-		//var a[]=arr;
-		//arr[arr.leth/2]+arr[arr.lenth/2+1]
-		var b=new Array(arr.length/2+1);
-		var j=0,n=0;
-		for(var i=0;i<arr.length;i++ ){
-
-			if(i%2==0){
-				b[j++]=arr[i];
-			}
-		}
-		if(j%2==0){
+	
+		if(arr.length==6){
 			return 4;
 		}
 		else{
